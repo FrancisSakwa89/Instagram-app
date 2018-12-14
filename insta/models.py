51e0a33f3cc9e15ffe0cab201dd64a5ebff99f71
sakwafrancis89@gmail.com <sakwafrancis89@gmail.com>
2018-12-14 11:15:27 +0300

    added config urls in instagram/urls.py

--- insta/models.py
+++ insta/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Image(models.Model):
@@ -6,7 +7,19 @@ class Image(models.Model):
    Image_name = models.CharField(max_length =30)
    Image_caption = models.TextField(max_length =40)
    Likes = models.CharField(max_length =20,blank =True)
-   profile = models.ForeignKey(Profile, null = True,related_name='image')
+#    Profile = models.ForeignKey(Profile, null = True,related_name='image')
    pub_date = models.DateTimeField(auto_now_add=True, null=True)
    comment = models.ForeignKey
    user= models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+
+class Profile(models.Model):
+   Profile_photo = models.ImageField(upload_to = 'images/',blank=True)
+   Bio = models.TextField(max_length = 50)
+#    user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)   
+
+class Like(models.Model):
+    user = models.ForeignKey(User)
+    profile = models.ForeignKey(Profile)
+    created = models.DateTimeField(auto_now_add=True)
+    # p = Profile.objects.get(...)
+    # number_of_likes = p.like_set.all().count()    
--- insta/views.py
+++ insta/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Image, Profile,Like
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 # Create your views here.
 def welcome(request):
     return render(request,'index.html')
+    
+def like(request, picture_id):
+    new_like, created = Like.objects.get_or_create(user=request.user, picture_id=picture_id)
+
+def picture_detail(request, id):
+    pic = get_object_or_404(Picture, pk=id)
+    user_likes_this = pic.like_set.filter(user=request.user) and True or False    
