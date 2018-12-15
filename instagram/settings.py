61811eb38a00e5f19a3977dc4ccac11a534ec9dc
sakwafrancis89@gmail.com <sakwafrancis89@gmail.com>
2018-12-15 10:50:00 +0300

    added email details in settings.py

--- instagram/settings.py
+++ instagram/settings.py
@@ -128,3 +128,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = 'welcome'
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'sakwafrancis89@gmail.com'
+EMAIL_HOST_PASSWORD = 'francis2017'
